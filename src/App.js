85030d624f5d7547c5a46a8f266c2d576312d181
Sachiiinn6249 <sachiiinn6249@gmail.com>
2024-04-05 18:32:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import styled from "styled-components";
 import './App.css';
 
-function App() {
+import IconLightbulb from "./Bulb";
+
+import CustomizedSwitches from "./Button";
+
+export default function App() {
+  const [Trigger, setTrigger] = useState(false);
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <Box style={{background:Trigger? "linear-gradient(to right, #283048, #859398)": ""}} className={Trigger? "light":""} >
+  <Box1>
+    <Box2/>
+  <IconLightbulb fill={ Trigger ? "#f5af19":"white"} power={Trigger ?  "#ffcc4d":"#696763" } name= {Trigger ? "animation": ""}/>
+
+  <Box3 onClick={() =>setTrigger(!Trigger)}>
+  <CustomizedSwitches  />
+  </Box3>
+  </Box1>
+  </Box>);
 }
 
-export default App;
+const Box = styled.div`
+ height: 100vh;
+ background: linear-gradient(to right, #000000, #434343);
+ display:flex;
+ justify-content: center;
+ 
+`;
+const Box1 = styled.div`
+ height: 450px;
+ width: 450px;
+`;
+
+const Box2 = styled.div`
+height: 50px;
+width:8px;
+background-color: gray;
+margin-left: 34%;
+
+`;
+
+const Box3 = styled.div`
+ margin-top: 100px;
+ margin-left: 120px;
+ width: 100px;
+ border-radius: 30%;
+`;
